f161a88cd0d0b0f1624585bc394feb9afa2b1bd1
Matthew Hobbs-Hurrell <matthewhobbshurrell@icloud.com>
2023-04-25 20:05:43 +0000

    Add RigDetail view with get, put and delete methods

--- rigs/views.py
+++ rigs/views.py
@@ -1,8 +1,10 @@
+from django.http import Http404
 from rest_framework import status, permissions
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import Rig
 from .serializers import RigSerializer
+from gear_addict_api.permissions import IsOwnerOrReadOnly
 
 
 class RigList(APIView):
@@ -32,4 +34,44 @@ class RigList(APIView):
         return Response(
             serializer.errors,
             status=status.HTTP_400_BAD_REQUEST
+        )
+
+
+class RigDetail(APIView):
+    serializer_class = RigSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+    
+    def get_object(self, pk):
+        try: 
+            rig = Rig.objects.get(pk=pk) 
+            self.check_object_permissions(self.request, rig)
+            return rig
+        except Rig.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        rig = self.get_object(pk) 
+        serializer=RigSerializer(
+            rig,
+            context= {'request' : request}
+        ) 
+        return Response(serializer.data)
+    
+    def put (self, request, pk):
+        rig = self.get_object(pk)
+        serializer=RigSerializer(
+            rig,
+            data=request.data,
+            context= {'request' : request}
+        )
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        rig = self.get_object(pk)
+        rig.delete()
+        return Response(
+            status=status.HTTP_204_NO_CONTENT
         )
