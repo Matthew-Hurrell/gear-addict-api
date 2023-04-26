76ee084c7169310c1e6e20a66c222ecb3a545f4f
Matthew Hobbs-Hurrell <matthewhobbshurrell@icloud.com>
2023-04-26 19:41:33 +0000

    Add GearList view with get method

--- gear/views.py
+++ gear/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import Gear
+from .serializers import GearSerializer
 
-# Create your views here.
+
+class GearList(APIView):
+    def get(self, request):
+        gear = Gear.objects.all()
+        serializer = GearSerializer(
+            gear, many = True, context = {'request': request}
+        )
+        return Response(serializer.data)
