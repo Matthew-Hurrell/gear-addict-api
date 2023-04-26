a9fc8afd10b9b58075079f9354745b5815c257a3
Matthew Hobbs-Hurrell <matthewhobbshurrell@icloud.com>
2023-04-26 20:04:35 +0000

    Add comment model

--- comments/models.py
+++ comments/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
+from rigs.models import Rig
 
-# Create your models here.
+
+class Comment(models.Model):
+    """
+    Comment model, related to User and Rig
+    """
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    rig = models.ForeignKey(Rig, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.content
+
