a7493a2dd1f77cd6af357358a4591cee5ab98d6d
Matthew Hobbs-Hurrell <matthewhobbshurrell@icloud.com>
2023-04-26 20:20:43 +0000

    Add CommentList and CommentDetail views

--- comments/views.py
+++ comments/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from gear_addict_api.permissions import IsOwnerOrReadOnly
+from .models import Comment
+from .serializers import CommentSerializer, CommentDetailSerializer
 
-# Create your views here.
+
+class CommentList(generics.ListCreateAPIView):
+    """
+    List all comments
+    Create a new comment if authenticated Associate the current logged in user with the
+    comment 
+    """
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Comment.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class CommentDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Retrieve a comment
+    update or delete a comment if owner
+    """
+    serializer_class = CommentDetailSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+    queryset = Comment.objects.all()
