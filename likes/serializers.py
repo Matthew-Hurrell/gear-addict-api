256f5b9880042f4eb0497cd65f9bba1674db13d5
Matthew Hobbs-Hurrell <matthewhobbshurrell@icloud.com>
2023-04-26 20:33:13 +0000

    Add LikeSerializer with create method

---
+++ likes/serializers.py
@@ -0,0 +1,26 @@
+from django.db import IntegrityError
+from rest_framework import serializers
+from .models import Like
+
+
+class LikeSerializer(serializers.ModelSerializer):
+    """
+    Serializer for Like model
+    The create method handles the unique constraint on
+    'owner' and 'rig'
+    """
+    owner = serializers.ReadOnlyField(source='owner.username')
+
+    class Meta:
+        model = Like
+        fields = [
+            'id', 'created_at', 'owner', 'rig'
+        ]
+
+    def create(self, validated_data):
+        try:
+            return super().create(validated_data)
+        except IntegrityError:
+            raise serializers.ValidationError({
+                'detail': 'possible duplicate'
+            })
