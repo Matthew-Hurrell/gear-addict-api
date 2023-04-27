2aeb7b1208d21fc2c2d3c1495838304554349007
Matthew Hobbs-Hurrell <matthewhobbshurrell@icloud.com>
2023-04-27 18:48:20 +0000

    Add savelist and savedetail class views for save model

--- save/views.py
+++ save/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from gear_addict_api.permissions import IsOwnerOrReadOnly
+from save.models import Save
+from save.serializers import SaveSerializer
 
-# Create your views here.
+
+class SaveList(generics.ListCreateAPIView):
+    """
+    List saves or add save if logged in
+    """
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    serializer_class = SaveSerializer
+    queryset = Save.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class SaveDetail(generics.RetrieveDestroyAPIView):
+    """
+    Retrieve a save. No Update view, as users can
+    only save or unsave a post. Destroy a save,
+    i.e. unsave a post if owner of that save
+    """
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = SaveSerializer
+    queryset = Save.objects.all()
