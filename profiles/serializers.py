88b332f4aa4b43dbcae9d96abccf78335fbd1a10
Matthew Hobbs-Hurrell <matthewhobbshurrell@icloud.com>
2023-04-27 21:11:42 +0000

    Add get fan id method to profile serializer

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -1,14 +1,34 @@
 from rest_framework import serializers
 from .models import Profile
+from fans.models import Fan
 
 
 class ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
+    fan_id = serializers.SerializerMethodField()
 
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
+
+    def get_fan_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            fanboy = Fan.objects.filter(
+               fan=user, idol=obj.owner 
+            ).first()
+            return fanboy.id if fanboy else None
+        return None
+
+    # def get_following_id(self, obj):
+    #     user = self.context['request'].user
+    #     if user.is_authenticated:
+    #         following = Follower.objects.filter(
+    #             owner=user, followed=obj.owner
+    #         ).first()
+    #         return following.id if following else None
+    #     return None
 
     class Meta: 
         model = Profile 
@@ -16,5 +36,5 @@ class ProfileSerializer(serializers.ModelSerializer):
             'id', 'owner', 'created_at', 'updated_at',
             'name', 'bio', 'image', 'header_image',
             'location', 'instruments', 'genres',
-            'expertise', 'is_owner'
+            'expertise', 'is_owner', 'fan_id'
         ]
