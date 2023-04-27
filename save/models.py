3331dc76171185a5b2fbf6fbbf75f4584709a4cc
Matthew Hobbs-Hurrell <matthewhobbshurrell@icloud.com>
2023-04-27 18:47:45 +0000

    Add class for Save model

--- save/models.py
+++ save/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
+from rigs.models import Rig
 
-# Create your models here.
+
+class Save(models.Model):
+    """
+    Save model, related to 'owner' and 'rig'
+    'owner' is a User instance and 'rig' is a Rig instance
+    'unique_together' makes sure a user can't save the same rig twice
+    """
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    rig = models.ForeignKey(
+        Rig,
+        related_name='save',
+        on_delete=models.CASCADE
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
+        unique_together = ['owner', 'rig']
+
+    def __str__(self):
+        return f'{self.owner} {self.rig}'
+
