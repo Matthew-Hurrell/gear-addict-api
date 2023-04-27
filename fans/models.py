10af8f1268a66906a5843d050c13d15dc09cb523
Matthew Hobbs-Hurrell <matthewhobbshurrell@icloud.com>
2023-04-27 19:11:47 +0000

    Add model class Fan

--- fans/models.py
+++ fans/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Fan(models.Model):
+    """
+    Fan model, related to 'fan' and 'idol'
+    We need the related_name attribute so that django can differentiate
+    between 'fan' and 'idol' who both are User model instances
+    'unique_together' makes sure a user can't double fan the same user
+    """
+    fan = models.ForeignKey(
+        User,
+        related_name='fan',
+        on_delete=models.CASCADE
+    )
+    idol = models.ForeignKey(
+        User,
+        related_name='idol',
+        on_delete=models.CASCADE
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
+        unique_together = ['fan', 'idol']
+
+    def __str__(self):
+        return f'{self.fan} {self.idol}'
