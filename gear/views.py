b89a02085a3315e11f3a873c8280344effe6a132
Matthew Hobbs-Hurrell <matthewhobbshurrell@icloud.com>
2023-04-27 20:37:47 +0000

    Refactor gear views to generic views

--- gear/views.py
+++ gear/views.py
@@ -1,82 +1,26 @@
-from django.http import Http404
-from rest_framework import status, permissions
-from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework import generics, permissions
+from gear_addict_api.permissions import IsOwnerOrReadOnly
 from .models import Gear
 from .serializers import GearSerializer
-from gear_addict_api.permissions import IsOwnerOrReadOnly
 
 
-class GearList(APIView):
+class GearList(generics.ListCreateAPIView):
+    """
+    List gear or create gear if logged in
+    The perform_create method associates the gear with the logged in user.
+    """
     serializer_class = GearSerializer
-    permission_classes = [
-        permissions.IsAuthenticatedOrReadOnly
-    ]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Gear.objects.all()
 
-    def get(self, request):
-        gear = Gear.objects.all()
-        serializer = GearSerializer(
-            gear, 
-            many = True,
-            context = {'request': request}
-        )
-        return Response(serializer.data)
-    
-    def post(self, request):
-        serializer = GearSerializer(
-            data=request.data,
-            context ={'request':request} 
-        )
-        if serializer.is_valid():
-            serializer.save(owner=request.user)
-            return Response(
-                serializer.data,
-                status=status.HTTP_201_CREATED
-            )
-        return Response( 
-            serializer.errors,
-            status=status.HTTP_400_BAD_REQUEST
-        )
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 
-class GearDetail(APIView):
+class GearDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Retrieve gear and edit or delete it if you own it.
+    """
     serializer_class = GearSerializer
     permission_classes = [IsOwnerOrReadOnly]
-    
-    def get_object(self, pk):
-        try: 
-            gear = Gear.objects.get(pk=pk) 
-            self.check_object_permissions(self.request, gear)
-            return gear
-        except Gear.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk):
-        gear = self.get_object(pk) 
-        serializer=GearSerializer(
-            gear,
-            context= {'request' : request}
-        ) 
-        return Response(serializer.data)
-    
-    def put(self, request, pk):
-        gear = self.get_object(pk)
-        serializer=GearSerializer(
-            gear,
-            data=request.data,
-            context= {'request' : request}
-        )
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(
-            serializer.errors,
-            status=status.HTTP_400_BAD_REQUEST
-        )
-
-    def delete(self, request, pk):
-        gear = self.get_object(pk)
-        gear.delete()
-        return Response(
-            status=status.HTTP_204_NO_CONTENT
-        )
+    queryset = Gear.objects.all()
