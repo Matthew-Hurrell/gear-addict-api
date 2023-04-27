6ff543c065f956296cb0a884eb63663d4864c26c
Matthew Hobbs-Hurrell <matthewhobbshurrell@icloud.com>
2023-04-27 19:12:50 +0000

    Add FanList and FanDetail class based views

--- fans/views.py
+++ fans/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from gear_addict_api.permissions import IsOwnerOrReadOnly
+from fans.models import Fan
+from fans.serializers import FanSerializer
 
-# Create your views here.
+
+class FanList(generics.ListCreateAPIView):
+    """
+    List all fans, i.e. all instances of user as a fan of
+    another user.
+    Create a fan, i.e. become a fan of a user if logged in
+    perform_create: associate the current logged in user with a fan
+    """
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    serializer_class = FanSerializer
+    queryset = Fan.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class FanDetail(generics.RetrieveDestroyAPIView):
+    """
+    Retrieve a fan
+    No Update view, as we either become a fan or unfan users
+    Destroy a fan, i.e. unfan someone if owner
+    """
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = FanSerializer
+    queryset = Fan.objects.all()
