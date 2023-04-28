c92d51b539e74c89469903845e8798c872e360a0
Matthew Hobbs-Hurrell <matthewhobbshurrell@icloud.com>
2023-04-28 19:51:50 +0000

    Add rigs count, fans, count, idols count and ordering to ProfileList view

--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,5 @@
-from rest_framework import generics
+from django.db.models import Count
+from rest_framework import generics, filters
 from gear_addict_api.permissions import IsOwnerOrReadOnly
 from .models import Profile
 from .serializers import ProfileSerializer
@@ -9,14 +10,31 @@ class ProfileList(generics.ListAPIView):
     List all profiles.
     No create view as profile creation is handled by django signals.
     """
-    queryset = Profile.objects.all()
+    queryset = Profile.objects.annotate(
+        rigs_count = Count('owner__rig', distinct=True),
+        fans_count = Count('owner__idolguy', distinct=True),
+        idols_count = Count('owner__fanboy', distinct=True)
+    ).order_by('-created_at')
     serializer_class = ProfileSerializer
-
+    filter_backends = [
+        filters.OrderingFilter
+    ]
+    ordering_fields = [
+        'rigs_count',
+        'fans_count',
+        'idols_count',
+        'owner__fanboy__created_at',
+        'owner__idolguy__created_at',
+    ]
 
 class ProfileDetail(generics.RetrieveUpdateAPIView):
     """
     Retrieve or update a profile if you're the owner.
     """
     permission_classes = [IsOwnerOrReadOnly]
-    queryset = Profile.objects.all()
+    queryset = Profile.objects.annotate(
+        rigs_count = Count('owner__rig', distinct=True),
+        fans_count = Count('owner__idolguy', distinct=True),
+        idols_count = Count('owner__fanboy', distinct=True)
+    ).order_by('-created_at')
     serializer_class = ProfileSerializer
