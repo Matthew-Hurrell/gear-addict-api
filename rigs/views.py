c9e112632915210cefa7b4eedfe90c13f70c0f4f
Matthew Hobbs-Hurrell <matthewhobbshurrell@icloud.com>
2023-04-29 13:42:19 +0000

    Import filter backend to rigs views and add filterset fields

--- rigs/views.py
+++ rigs/views.py
@@ -1,5 +1,6 @@
 from django.db.models import Count
 from rest_framework import generics, permissions, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from gear_addict_api.permissions import IsOwnerOrReadOnly
 from .models import Rig
 from .serializers import RigSerializer
@@ -20,6 +21,15 @@ class RigList(generics.ListCreateAPIView):
     filter_backends = [
         filters.OrderingFilter,
         filters.SearchFilter,
+        DjangoFilterBackend,
+    ]
+    filterset_fields = [
+        # User feed
+        'owner__idolguy__fan__profile',
+        # User liked rigs
+        'likes__owner__profile',
+        # User rigs
+        'owner__profile',
     ]
     search_fields = [
         'owner__username',
