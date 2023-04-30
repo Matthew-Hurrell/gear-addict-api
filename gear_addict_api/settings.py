f604e0906cfe176c9364ca4818f1768ed71ad0af
Matthew Hobbs-Hurrell <matthewhobbshurrell@icloud.com>
2023-04-30 16:20:54 +0000

    Add site_id variable to settings.py file

--- gear_addict_api/settings.py
+++ gear_addict_api/settings.py
@@ -65,6 +65,8 @@ INSTALLED_APPS = [
     'fans',
 ]
 
+SITE_ID = 1
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
